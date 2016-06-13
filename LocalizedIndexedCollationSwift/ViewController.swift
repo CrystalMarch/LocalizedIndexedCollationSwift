5e0a534b6032e5d78fc1d4158b4e691e4252f329
Crystal <zhuhuiping@zhuhuipingdemac-mini.local>
2016-06-13 14:43:11 +0800

    Initial Commit

---
+++ LocalizedIndexedCollationSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LocalizedIndexedCollationSwift
+//
+//  Created by 朱慧平 on 16/6/13.
+//  Copyright © 2016年 朱慧平. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
